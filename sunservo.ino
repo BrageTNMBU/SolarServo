f634a3ab6c66f4f472bc620fe1c7cf29dc40c308
BrageTNMBU <89648934+BrageTNMBU@users.noreply.github.com>
2021-08-29 23:22:45 +0200

    Update sunservo.ino

--- sunservo.ino
+++ sunservo.ino
@@ -5,10 +5,12 @@
 
 //DEFINES - USER MODIFIABLE------------------------------------
 #define DELAYINHOURS          0.0003 //Hours between each new rotation
-#define LUXCUTOFF             700    //For which lux value the system will assume night time (SKRIV HVA SOM ER MAKS MIN OG HVA SOM ER MER MINDRE)
-#define STEPS                 3      //How many steps the sensor will search in each direction when the assumed optimal position is found
-#define LUXDIFERENCETOLERANCE 10     //How much difference in lux the two sensors can have before deciding to move in either direction (EXCLUSIVE)
+#define LUXCUTOFF             200    //For which lux value the system will assume night time (SKRIV HVA SOM ER MAKS MIN OG HVA SOM ER MER MINDRE)
+#define LUXDIFERENCETOLERANCE 10     //How much difference in lux the two sensors can have before deciding to move in either direction (EXCLUSIVE)! CANNOT BE 0!
 #define SERVO_PIN             9      //Which pin the servo signal is connected to 
+#define LEFT_PHOTORES         A0     //Which pin left photoresistor is connected to
+#define RIGHT_PHOTORES        A1     //Which pin right photoresistor is connected to
+#define GLOBAL_PHOTORES       A2     //Which pin global photoresistor is connected to
 //DO NOT MODIFY BEYOND THIS LINE
 
 //INCLUDES----------------------------------------------------
@@ -16,7 +18,7 @@
 
 //VARIABLE DEFINITIONS
 Servo Panelservo;
-int angle = 90;
+byte angle = 90;
 enum Control
 {
   NONE, LEFT, STAY, RIGHT
@@ -26,28 +28,56 @@ enum Control
 //FUNCTIONS---------------------------------------------------
 void setup() //Setup function 
 {
-  Serial.begin(9600);
-  Panelservo.attach(SERVO_PIN); //Attaches servo to pin
-  Panelservo.write(angle);      //Moves servo to 90 degrees NOTE: THIS WILL HAPPEN SUDDENLY AND MIGHT DAMAGE PANEL IF CONNECTED DURING STARTUP
-  delay(2000);                  //Delay before starting to prevent sudden movement after startup
+  Serial.begin(9600);             //Initialization
+  Panelservo.attach(SERVO_PIN);   //Attaches servo to pin
+  Panelservo.write(angle);        //Moves servo to 90 degrees NOTE: THIS WILL HAPPEN SUDDENLY AND MIGHT DAMAGE PANEL IF CONNECTED DURING STARTUP
+  pinMode(LEFT_PHOTORES, INPUT);  //Sets analog pin to input
+  pinMode(RIGHT_PHOTORES, INPUT); //Sets analog pin to input
+  pinMode(GLOBAL_PHOTORES, INPUT);//Sets analog pin to input
+  delay(2000);                    //Delay before starting to prevent sudden movement after startup
 }
 
 bool checkfordaytime()
 {
-  return true;
+  int value = analogRead(GLOBAL_PHOTORES); //Reads light from global photoresistor
+  if (value > LUXCUTOFF) //If this light is greater then preset cutoffvalue
+  {
+    return true;         //There is daylight outside
+  }
+  else
+  {
+    return false;       //It is night time
+  }
 }
 void errorHandle() //Called in case the code can't determine which direction to move
 {
-  Serial.println(""); //Debug message
+  Serial.println("NONE WAS RETURNED!"); //Debug message
 }
 Control directionofmostsunlight()
 {
-  return LEFT;
-
+  int right = analogRead(RIGHT_PHOTORES); //Reads light from right photoresistor
+  int left = analogRead(LEFT_PHOTORES);   //Reads light from left photoresistor
+  int difference = abs(right-left);       //Finds difference between right and left photoresistor
+  if (difference < LUXDIFERENCETOLERANCE) //Checks if difference between right and left is within tolerance 
+  {
+    return STAY;
+  }
+  else if (right > left)
+  {
+    return RIGHT;
+  }
+  else if (left > right)
+  {
+    return LEFT;
+  }
+  if (right == left)
+  {
+    Serial.print("RIGHT = LEFT BUT NONE IS RETURNED! MISTAKE IN FUNCTION directionofmostsunlight"); //Debug message. This will return none and should not be reached due to difference check 
+  }
   return NONE; //This should not happen
 }
 
-void moveLeft(Control* modelControl, int* iterations) //Movement to the left
+void moveLeft(Control* modelControl, byte* iterations) //Movement to the left
 {
   if (angle<=180) //Prevents servo from moving further than servo range of motion
   {
@@ -58,7 +88,7 @@ void moveLeft(Control* modelControl, int* iterations) //Movement to the left
   movement(modelControl, iterations); //Recursivly calls movement()
 }
 
-void moveRight(Control* modelControl, int* iterations) //Movement to the right
+void moveRight(Control* modelControl, byte* iterations) //Movement to the right
 {
   if (angle>10) //Prevents servo from moving further than servo range of motion
   {
@@ -69,7 +99,7 @@ void moveRight(Control* modelControl, int* iterations) //Movement to the right
   movement(modelControl, iterations); //Recursivly calls movement()
   
 }
-void movement(Control* modelControl, int* iterations) //Calls which direction to move after checking the direction of most sunlight
+void movement(Control* modelControl, byte* iterations) //Calls which direction to move after checking the direction of most sunlight
 {
   *iterations+=1; //Increments iterations
   delay(100);     //Delay to prevent the servo from moving to fast and damaging the model
@@ -81,25 +111,34 @@ void movement(Control* modelControl, int* iterations) //Calls which direction to
 
     *modelControl = directionofmostsunlight(); //Tells the modelcontrol which direction the panel needs to move
     
-    if (*modelControl == LEFT) 
+    if (*modelControl == LEFT)
+    { 
       moveLeft(modelControl, iterations); //Moves left if left side has more sunlight
-    else if (*modelControl == STAY){}     //Does nothing if both sides have equal amount of sunlight
+    }
+    else if (*modelControl == STAY)       
+    {
+      Serial.println("STAY");            //Prints STAY if both sides have about equal amount of sunlight
+    }
     else if (*modelControl == RIGHT)
+    {
       moveRight(modelControl, iterations); //Moves right if right side has more sunlight
+    }
     else
+    {
       errorHandle();                       //If modelcontrol is not set to anything, something is wrong
+    }
   }
 }
 
 
 void loop() //Arduino loop function
 {
-  // put your main code here, to run repeatedly:
+
   bool isDayTime = checkfordaytime(); //Returns whether it is day or night
   
   if (isDayTime) //Executes only if it is daytime
   {
-    int iterations = 0; //Measures how often movement() is called
+    byte iterations = 0; //Measures how often movement() is called
     
     Control modelControl; //Creates new modelcontrol which will be deleted after iteration
 
